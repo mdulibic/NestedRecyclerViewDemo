c1dc361e0d2574ee1e564b03daf25ee03bb1cca9
mdulibic <dulibicmarta00@gmail.com>
2022-07-29 00:34:29 +0200

    Inital commit

---
+++ app/src/main/java/studio/codable/nestedrecyclerviewdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package studio.codable.nestedrecyclerviewdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
