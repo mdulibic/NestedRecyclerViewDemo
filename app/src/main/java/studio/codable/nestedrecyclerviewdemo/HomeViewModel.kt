d840bcfa051edeaf215a3219b22c2ac9507b3493
mdulibic <dulibicmarta00@gmail.com>
2022-08-22 09:37:14 +0200

    move helper fun to view model

--- app/src/main/java/studio/codable/nestedrecyclerviewdemo/HomeViewModel.kt
+++ app/src/main/java/studio/codable/nestedrecyclerviewdemo/HomeViewModel.kt
@@ -4,15 +4,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import studio.codable.nestedrecyclerviewdemo.adapter.model.ViewTypeItem
+import studio.codable.nestedrecyclerviewdemo.model.Palette
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel : ViewModel() {
 
     private val _paletteListHomeData = MutableLiveData<List<ViewTypeItem>>()
     val paletteListHomeData: LiveData<List<ViewTypeItem>>
         get() = _paletteListHomeData
 
-    fun getPaletteList(){
+    fun getPaletteList() {
         _paletteListHomeData.value = getTestData().toViewTypeItemList()
     }
 
+    private fun List<Palette>.toViewTypeItemList(): List<ViewTypeItem> {
+        val listItems: ArrayList<ViewTypeItem> = arrayListOf()
+        this.forEach {
+            listItems.add(ViewTypeItem.HeaderView(title = it.name))
+            listItems.add(ViewTypeItem.ColorListView(colorItems = it.colorItems))
+        }
+        return listItems
+    }
 }
