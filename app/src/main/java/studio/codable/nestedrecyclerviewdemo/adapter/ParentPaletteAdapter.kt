70fa9a2df60b0c09a9afa98eb1ff948cbb903615
mdulibic <dulibicmarta00@gmail.com>
2022-08-31 13:55:14 +0200

    refactor view holders to extend custom vh sealed class

--- app/src/main/java/studio/codable/nestedrecyclerviewdemo/adapter/ParentPaletteAdapter.kt
+++ app/src/main/java/studio/codable/nestedrecyclerviewdemo/adapter/ParentPaletteAdapter.kt
@@ -7,36 +7,34 @@ import androidx.recyclerview.widget.RecyclerView
 import studio.codable.nestedrecyclerviewdemo.adapter.model.ViewTypeItem
 import studio.codable.nestedrecyclerviewdemo.adapter.model.ViewTypeItem.ColorListView
 import studio.codable.nestedrecyclerviewdemo.adapter.model.ViewTypeItem.HeaderView
-import studio.codable.nestedrecyclerviewdemo.adapter.viewHolder.ColorListVH
-import studio.codable.nestedrecyclerviewdemo.adapter.viewHolder.HeaderVH
 import studio.codable.nestedrecyclerviewdemo.databinding.ItemHeaderBinding
 import studio.codable.nestedrecyclerviewdemo.databinding.LayoutColorItemListBinding
 
 class ParentPaletteAdapter(
     private var childItems: List<ViewTypeItem> = arrayListOf()
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<VH>() {
 
     companion object {
         const val VIEW_TYPE_CHILD_ITEM = 12
         const val VIEW_TYPE_HEADER = 13
-        const val PARENT_PALETTE_ADAPTER = "ParentPaletteAdapter"
+        const val LOG_TAG = "ParentPaletteAdapter"
     }
 
     fun update(newItems: List<ViewTypeItem>) {
-        Log.d(PARENT_PALETTE_ADAPTER, "Items: $newItems")
+        Log.d(LOG_TAG, "Items: $newItems")
         childItems = newItems
         notifyDataSetChanged()
     }
 
     override fun getItemViewType(position: Int): Int = childItems[position].viewType
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         val context = parent.context
-        Log.d(PARENT_PALETTE_ADAPTER, "View type $viewType")
+        Log.d(LOG_TAG, "View type $viewType")
 
         return when (viewType) {
             VIEW_TYPE_HEADER -> {
-                HeaderVH(
+                VH.HeaderVH(
                     ItemHeaderBinding.inflate(
                         LayoutInflater.from(context),
                         parent,
@@ -45,7 +43,7 @@ class ParentPaletteAdapter(
                 )
             }
             VIEW_TYPE_CHILD_ITEM -> {
-                ColorListVH(
+                VH.ColorListVH(
                     LayoutColorItemListBinding.inflate(
                         LayoutInflater.from(context),
                         parent,
@@ -53,16 +51,16 @@ class ParentPaletteAdapter(
                     )
                 )
             }
-            else -> throw IllegalStateException("Unknown view type")
+            else -> throw IllegalStateException("Unknown view type: $viewType")
         }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: VH, position: Int) {
         val item = childItems[position]
 
         when (holder) {
-            is ColorListVH -> holder.bind(item as ColorListView)
-            is HeaderVH -> holder.bind(item as HeaderView)
+            is VH.ColorListVH -> holder.bind(item as ColorListView)
+            is VH.HeaderVH -> holder.bind(item as HeaderView)
         }
     }
 
